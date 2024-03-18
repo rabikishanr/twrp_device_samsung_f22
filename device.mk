520cfd73da65052ba1ef3467a4b2cf633148ea15
Rabikishan Rauniyar <154669639+rabikishanr@users.noreply.github.com>
2024-03-18 14:44:08 +0545

    F22: fixes

--- device.mk
+++ device.mk
@@ -1,2 +1,8 @@
-
 LOCAL_PATH := device/samsung/f22
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
